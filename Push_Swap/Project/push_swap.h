a95beb064d27651d91ba407620042662a58a6baf
mzanana <mr.zananamohamed@gmail.com>
2025-03-20 04:29:26 +0000

    Declaration of the first four operations of the stacks

--- Push_Swap/Project/push_swap.h
+++ Push_Swap/Project/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 04:02:49 by mzanana           #+#    #+#             */
-/*   Updated: 2025/03/20 04:06:26 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/03/20 04:28:33 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,14 @@ typedef struct s_stack
     struct s_stack *next;
 }t_stack;
 
+// operations_1 : sa, sb, ss, pa, pb.
+void    sa(t_stack **st);
+void    sb(t_stack **st);
+void    ss(t_stack **st1, t_stack **st2);
+void    pa(t_stack **st1, t_stack **st2);
+void    pb(t_stack **st1, t_stack **st2);
 
+// operations_2 : ra, rb, rr.
+
+// operations_3 : rra, rrb, rrr.
 #endif
