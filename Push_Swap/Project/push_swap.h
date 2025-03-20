e698f1bfba9a63d6d1a77d8fe8aea12b5c4d5b91
mzanana <mr.zananamohamed@gmail.com>
2025-03-20 04:06:54 +0000

    create the stack struct

--- Push_Swap/Project/push_swap.h
+++ Push_Swap/Project/push_swap.h
@@ -6,7 +6,18 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 04:02:49 by mzanana           #+#    #+#             */
-/*   Updated: 2025/03/20 04:02:50 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/03/20 04:06:26 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_stack
+{
+    int data;
+    struct s_stack *next;
+}t_stack;
+
+
+#endif
