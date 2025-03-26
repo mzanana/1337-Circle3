040e940eeb2b45f10891a32343ac7e436bc9f03b
mzanana <mr.zananamohamed@gmail.com>
2025-03-26 23:46:16 +0000

    Start the split function (count word function)

--- Push_Swap/Project/parsing.c
+++ Push_Swap/Project/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 21:23:24 by mzanana           #+#    #+#             */
-/*   Updated: 2025/03/26 23:41:19 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/03/26 23:42:49 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,43 @@ int is_valid(char *str)
 	}
 	return 1;
 }
+int ft_countword(char *str)
+{
+	int i;
+	int ret;
+	int flag;
 
+	i = 0;
+	while (str[i] && str[i] == ' ')
+		i++;
+	ret = 0;
+	// if (str[i])
+	// 	ret++;
+	flag = 1;
+	while (str[i])
+	{
+		if (str[i] != ' ' && flag == 1)
+		{
+			flag = 0;
+			ret++;
+		}
+		else if (str[i] == ' ' && flag == 0)
+			flag = 1;
+		i++;
+	}
+	return ret;
+}
+char **ft_split(char *str)
+{
+	int		i;
+	char	**ret;
+
+	i = 0;
+	while (str[i] && str[i] == ' ')
+		i++;
+	ret = malloc (sizeof(char*) * (ft_countword(str) + 1));
+	
+}
 
 t_stack *ft_parsing(char **av, int ac)
 {
@@ -48,6 +84,7 @@ t_stack *ft_parsing(char **av, int ac)
 	{
 		if (!is_valid(av[i]))
 			return NULL;
+		sp = ft_split(av[i]);
 	}
 	
 }
