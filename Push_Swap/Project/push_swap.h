2bef94005e3556dfe3ebb584fa48f1e9f0d9d3b2
mzanana <mr.zananamohamed@gmail.com>
2025-03-27 22:04:17 +0000

    Update the header file

--- Push_Swap/Project/push_swap.h
+++ Push_Swap/Project/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 04:02:49 by mzanana           #+#    #+#             */
-/*   Updated: 2025/03/27 03:43:03 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/03/27 22:03:03 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,21 @@ void    pb(t_stack **st1, t_stack **st2);
 
 // operations_3 : rra, rrb, rrr.
 
-// helpers
-char    **ft_split(char *str);
-void    jump_spaces(char *str, int *i);
-void    free_split(char **str);
-int     ft_stringlen(char *str, int start);
-int     ft_countword(char *str);
-
-void ft_error(char **str, t_stack *st);
-void ft_putstr(char *str);
-int ft_strlen(char *str);
-
+// parsing.c
+t_stack*    ft_parsing(char **av, int ac);
+int         ft_atoi(char *str, char **split, t_stack *stack);
+int         is_valid(char *str);
+int         is_num(char c);
+void        ft_error(char **str, t_stack *st);
+//ft_split.c
+char**      ft_split(char *str);
+void        jump_spaces(char *str, int *i);
+void        free_split(char **str);
+int         ft_stringlen(char *str, int start);
+int         ft_countword(char *str);
+// helpers1.c
+void        ft_putstr(char *str);
+int         ft_strlen(char *str);
+int         num_duplicate(t_stack *stack, int num);
+int         check_number(char *str);
 #endif
