7a9d5cb6d90bc01658a2024befb080e722578ca3
mzanana <mr.zananamohamed@gmail.com>
2025-03-28 21:52:33 +0000

    adding ft_push to the header file

--- Push_Swap/Project/push_swap.h
+++ Push_Swap/Project/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 04:02:49 by mzanana           #+#    #+#             */
-/*   Updated: 2025/03/28 20:45:31 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/03/28 21:49:22 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,11 @@ void    sa(t_stack **stack);
 void    sb(t_stack **stack);
 void	ss(t_stack **sta, t_stack **stb);
 void    pa(t_stack **sta, t_stack **stb);
-void    pb(t_stack **sta, t_stack **stb);
+void    pb(t_stack **stb, t_stack **sta);
 
 // operations_2 : ra, rb, rr.
 void	ft_swap(t_stack **stack);
+void	ft_push(t_stack **sta, t_stack **stb);
 
 // operations_3 : rra, rrb, rrr.
 
