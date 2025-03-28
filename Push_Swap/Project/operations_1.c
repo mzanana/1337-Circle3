d4d4c9bd8ea116929a895431f6c2538c29361200
mzanana <mr.zananamohamed@gmail.com>
2025-03-28 17:23:36 +0000

    Swap a stack function

--- Push_Swap/Project/operations_1.c
+++ Push_Swap/Project/operations_1.c
@@ -6,7 +6,22 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:40:58 by mzanana           #+#    #+#             */
-/*   Updated: 2025/03/28 16:41:00 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/03/28 17:15:34 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "push_swap.h"
+// sa, sb, ss, pa, pb.
+
+void	sa(t_stack **stack)
+{
+	t_stack *tmp;
+	
+	if (!stack || !(*stack) || !(*stack)->next)
+		return ;
+	tmp = *stack;
+	*stack = (*stack)->next;
+	tmp->next = (*stack)->next;
+	(*stack)->next = tmp;
+}
 
