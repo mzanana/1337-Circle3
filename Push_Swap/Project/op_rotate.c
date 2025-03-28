986f010640cbe2b5a0649b7931c3084ce7ce72d7
mzanana <mr.zananamohamed@gmail.com>
2025-03-28 23:59:12 +0000

    reverse rotate stack a and stack b

--- Push_Swap/Project/op_rotate.c
+++ Push_Swap/Project/op_rotate.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:41:36 by mzanana           #+#    #+#             */
-/*   Updated: 2025/03/28 23:37:54 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/03/28 23:58:29 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,16 @@ void	rr(t_stack **sta, t_stack **stb)
 	write (1, "rr\n", 3);
 	ft_rotate(sta);
 	ft_rotate(stb);
+}
+
+void	rra(t_stack **stack)
+{
+	write (1, "rra\n", 4);
+	ft_reverse_rot(stack);
+}
+
+void	rrb(t_stack **stack)
+{
+	write (1, "rrb\n", 4);
+	ft_reverse_rot(stack);
 }
