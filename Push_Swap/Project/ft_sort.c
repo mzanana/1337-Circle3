8e83be29a1e940d817e7f5cab6228bf2542e6a63
mzanana <mr.zananamohamed@gmail.com>
2025-03-29 00:21:00 +0000

    create the function of reverse rotate fot the both stack a and b at the same time

--- Push_Swap/Project/ft_sort.c
+++ Push_Swap/Project/ft_sort.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_sort.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/20 04:02:43 by mzanana           #+#    #+#             */
-/*   Updated: 2025/03/29 00:13:36 by mzanana          ###   ########.fr       */
+/*   Created: 2025/03/29 00:09:59 by mzanana           #+#    #+#             */
+/*   Updated: 2025/03/29 00:10:43 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int ac, char **av)
+void	ft_sort(t_stack **sta, t_stack **stb)
 {
-    t_stack *sta;
-    t_stack *stb;
-    int     size;
-
-    if (ac < 2)
-        return (0);
-    stb = NULL;
-    sta = ft_parsing(av, ac);
-    if (!sta)
-        ft_error(NULL, NULL);
-    if (is_sorted(sta))
-        (free_stack(&sta), exit(0));
-    size = ft_size(sta);
-    ft_sort(&sta, &stb, size);
-    free_stack(sta);
+	
 }
-
-
