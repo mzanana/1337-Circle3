5e63dec530b653de8d194233daf451e78fef949a
mzanana <mr.zananamohamed@gmail.com>
2025-03-29 00:24:56 +0000

    on main see if the stack is sorted or not, and calculate the size of the stack

--- Push_Swap/Project/main.c
+++ Push_Swap/Project/main.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 04:02:43 by mzanana           #+#    #+#             */
-/*   Updated: 2025/03/28 23:39:17 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/03/29 00:13:36 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@ int main(int ac, char **av)
 {
     t_stack *sta;
     t_stack *stb;
+    int     size;
 
     if (ac < 2)
         return (0);
     stb = NULL;
     sta = ft_parsing(av, ac);
     if (!sta)
-    {
         ft_error(NULL, NULL);
-        return 1;
-    } 
+    if (is_sorted(sta))
+        (free_stack(&sta), exit(0));
+    size = ft_size(sta);
+    ft_sort(&sta, &stb, size);
+    free_stack(sta);
 }
 
 
