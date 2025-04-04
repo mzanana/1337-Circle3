3578e9675d54c2bbc1e2d62090bce1f391486fc5
mzanana <mr.zananamohamed@gmail.com>
2025-04-04 21:38:02 +0000

    the function to get the positon of the minimum number at the current stack

--- Push_Swap/Project/sorting.c
+++ Push_Swap/Project/sorting.c
@@ -6,20 +6,44 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 01:53:45 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/04 21:34:05 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/04 21:37:21 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int		min_pos(t_stack *st)
+{
+	int pos, i, min;
+	t_stack *tmp;
+
+	i = 0;
+	pos = 0;
+	min = st->data;
+	tmp = st;
+	while (tmp)
+	{
+		if (tmp->data < min)
+		{
+			pos = i;
+			min = tmp->data;
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	return pos;
+}
 
 
 void    easy_sorting(t_stack **sta, t_stack **stb)
 {
-	int (a_size);
+	int (a_size), (pos);
 
 	a_size = lst_size(*sta); 
 	if (a_size == 2)
 		sa(sta);
-	
+	while (a_size > 3)
+	{
+		pos = min_pos(sta);
+	}
 }
