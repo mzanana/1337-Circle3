364cf596bdda1c95cdcf7c726969f7e39c88f783
mzanana <mr.zananamohamed@gmail.com>
2025-04-05 03:24:20 +0000

    fill the array with the same numbers of the stack

--- Push_Swap/Project/chunks_sorting.c
+++ Push_Swap/Project/chunks_sorting.c
@@ -6,19 +6,35 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 01:04:00 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/05 03:22:40 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/05 03:23:57 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int		*fill_arr(t_stack *sta)
+{
+	int *ret;
+	int i, size;
+	t_stack *tmp;
 
-
+	size = lst_size(sta);
+	ret = malloc (sizeof(int) * size);
+	if (!ret)
+		return NULL;
+	tmp = sta;
+	i = 0;
+	while (tmp)
+	{
+		ret[i++] = tmp->data;
+		tmp = tmp->next;
+	}
+	return ret;
+}
 void    chunks_sorting(t_stack **sta, t_stack **stb)
 {
 	int	(min), (max), (size), (holder);
 	int	*arr;
 
 	arr = fill_arr(*sta);
-	size = lst_size(*sta);
 }
