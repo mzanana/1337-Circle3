2bf671d18983ad7fd42ac16b2d6aaa20095e7a3b
mzanana <mr.zananamohamed@gmail.com>
2025-04-05 03:23:34 +0000

    the chunks sorting algo and thinking of filling array sorted

---
+++ Push_Swap/Project/chunks_sorting.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chunks_sorting.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 01:04:00 by mzanana           #+#    #+#             */
+/*   Updated: 2025/04/05 03:22:40 by mzanana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+
+
+void    chunks_sorting(t_stack **sta, t_stack **stb)
+{
+	int	(min), (max), (size), (holder);
+	int	*arr;
+
+	arr = fill_arr(*sta);
+	size = lst_size(*sta);
+}
