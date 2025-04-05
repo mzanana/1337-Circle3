1160e633343191de2a3749aa4ca4d1cec400a242
mzanana <mr.zananamohamed@gmail.com>
2025-04-05 21:16:00 +0000

    the function that help to push the sorted data to stack a after the right operations

---
+++ Push_Swap/Project/chunks_helper.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chunks_helper.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 04:17:44 by mzanana           #+#    #+#             */
+/*   Updated: 2025/04/05 06:06:55 by mzanana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int 	max_pos(t_stack *stb)
+{
+	int pos, i, max;
+	t_stack *tmp;
+
+	i = 0;
+	pos = 0;
+	tmp = stb;
+	max = tmp->data;
+	while (tmp)
+	{
+		if (tmp->data > max)
+		{
+			max = tmp->data;
+			pos = i;
+		}
+		i++;
+		tmp = tmp->next;
+	}
+	return pos;
+}
+void 	push_back_sta(t_stack **sta, t_stack **stb)
+{
+	int pos, b_size;
+
+	b_size = lst_size(*stb);
+	while (b_size > 0)
+	{
+		pos = max_pos(*stb);
+		if (pos > b_size / 2)
+			while (pos++ < b_size)
+				rrb(stb);
+		else if (pos <= b_size / 2)
+			while (pos-- > 0)
+				rb(stb);
+		pa(sta, stb);
+		b_size--;
+	}
+}
