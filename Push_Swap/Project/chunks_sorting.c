03e5ca06419d3d0c6b715af879863f2c7aae8f0d
mzanana <mr.zananamohamed@gmail.com>
2025-04-05 03:25:07 +0000

    sort the array to be worked fine for the chunks algo

--- Push_Swap/Project/chunks_sorting.c
+++ Push_Swap/Project/chunks_sorting.c
@@ -6,11 +6,33 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 01:04:00 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/05 03:23:57 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/05 03:24:44 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void 	sort_arr(int *arr, int size)
+{
+	int i, swap, j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i;
+		while (j < size)
+		{
+			if (arr[i] > arr[j])
+			{
+				swap = arr[i];
+				arr[i] = arr[j];
+				arr[j] = swap;
+			}
+			j++;
+		}
+		i++;
+	}
+}
 
 int		*fill_arr(t_stack *sta)
 {
@@ -29,6 +51,7 @@ int		*fill_arr(t_stack *sta)
 		ret[i++] = tmp->data;
 		tmp = tmp->next;
 	}
+	sort_arr(ret, size);
 	return ret;
 }
 void    chunks_sorting(t_stack **sta, t_stack **stb)
