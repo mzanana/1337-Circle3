ca6ae263f52eb0e5509c329918aa2b848e9660a6
mzanana <mr.zananamohamed@gmail.com>
2025-04-05 23:51:55 +0000

    norminette main.c

--- Push_Swap/Project/main.c
+++ Push_Swap/Project/main.c
@@ -6,32 +6,30 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 04:02:43 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/05 21:14:20 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/05 23:51:42 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    t_stack *sta;
-    t_stack *stb;
-    int     size;
+	t_stack		*sta;
+	t_stack		*stb;
+	int			size;
 
-    if (ac < 2)
-        return (0);
-    stb = NULL;
-    sta = ft_parsing(av, ac);
-    if (!sta)
-        ft_error(NULL, NULL);
-    if (is_sorted(sta))
-        return (free_stack(&sta), 0);
-    size = lst_size(sta);
-    if (size <= 10)
-        easy_sorting(&sta, &stb);
-    else
-        chunks_sorting(&sta, &stb);
-    free_stack(&sta);
+	if (ac < 2)
+		return (0);
+	stb = NULL;
+	sta = ft_parsing(av, ac);
+	if (!sta)
+		ft_error(NULL, NULL);
+	if (is_sorted(sta))
+		return (free_stack(&sta), 0);
+	size = lst_size(sta);
+	if (size <= 10)
+		easy_sorting(&sta, &stb);
+	else
+		chunks_sorting(&sta, &stb);
+	free_stack(&sta);
 }
-
-
