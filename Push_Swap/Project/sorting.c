edb55a1f2d309f12059f042a4d8383d83fdb01cb
mzanana <mr.zananamohamed@gmail.com>
2025-04-05 00:47:40 +0000

    create the function that sort three numbers at the stack and make the values returned to the stack original a

--- Push_Swap/Project/sorting.c
+++ Push_Swap/Project/sorting.c
@@ -6,21 +6,38 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 01:53:45 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/04 21:39:17 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/05 00:45:52 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	three_sort(t_stack **st)
+{
+	int (f), (s), (t);
+	
+	f = (*st)->data;
+	s = (*st)->next->data;
+	t = (*st)->next->next->data;
+	
+	if (f > s && f > t)
+		ra(st);
+	if (s > f && s > t)
+		rra(st);
+	if ((*st)->data > (*st)->next->data)
+		sa(st);
+}
 
 int		min_pos(t_stack *st)
 {
 	int pos, i, min;
 	t_stack *tmp;
 
-	i = 0;
-	pos = 0;
+	
 	min = st->data;
 	tmp = st;
+	i = 0;
+	pos = 0;
 	while (tmp)
 	{
 		if (tmp->data < min)
@@ -31,28 +48,33 @@ int		min_pos(t_stack *st)
 		tmp = tmp->next;
 		i++;
 	}
+	// printf("%d\n", pos);
 	return pos;
+	
 }
 
 void    easy_sorting(t_stack **sta, t_stack **stb)
 {
-	int (a_size), (pos);
-
+	int (a_size), (pos), (b_size);
+	
 	a_size = lst_size(*sta); 
 	if (a_size == 2)
 		sa(sta);
 	while (a_size > 3)
 	{
-		pos = min_pos(sta);
-		if (pos <= (a_size/2))
-		{
+		pos = min_pos(*sta);
+		if (pos <= a_size / 2)
 			while (pos-- > 0)
 				ra(sta);
-		}
 		else
 			while (pos++ < a_size)
 				rra(sta);
+		pb(stb, sta);
 		a_size--;
 	}
+	three_sort(sta);
+	b_size = lst_size(*stb);
+	while (b_size-- > 0)
+		pa(sta, stb);
 }
 
