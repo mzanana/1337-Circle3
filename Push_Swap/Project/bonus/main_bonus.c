0d279416dc40dfaf74f1914777b119886fa2748a
mzanana <mr.zananamohamed@gmail.com>
2025-04-06 06:01:14 +0100

    create the apply_oper to check if the operations are valid

--- Push_Swap/Project/bonus/main_bonus.c
+++ Push_Swap/Project/bonus/main_bonus.c
@@ -6,11 +6,32 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 05:23:50 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/06 05:59:31 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/06 06:00:44 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker_bonus.h"
+
+void	apply_oper(t_stack **sta, t_stack **stb)
+{
+	char	*op;
+
+	op = get_next_line(0);
+	while (op)
+	{
+		if (!execute(op, sta, stb))
+		{
+			free(op);
+			write(2, "Error\n", 6);
+			get_next_line(-1);
+			free_stack(sta);
+			free_stack(stb);
+			exit(1);
+		}
+		free(op);
+		op = get_next_line(0);
+	}
+}
 
 int	main(int ac, char **av)
 {
