b9192983cb542f920e7a1cca804e251cb1efeb65
mzanana <mr.zananamohamed@gmail.com>
2025-04-06 07:00:00 +0100

    fix typo errors

--- Push_Swap/Project/bonus/main_bonus.c
+++ Push_Swap/Project/bonus/main_bonus.c
@@ -6,26 +6,12 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 05:23:50 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/06 06:04:02 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/06 06:50:14 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker_bonus.h"
 
-int	ft_strcmp(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i] && s2[i])
-	{
-		if (s1[i] == s2[i])
-			i++;
-		else
-			return (s1[i] - s2[i]);
-	}
-	return (s1[i] - s2[i]);
-}
 
 int	execute(char *oper, t_stack **sta, t_stack **stb)
 {
@@ -81,8 +67,8 @@ void	result(t_stack *stack_a, t_stack *stack_b)
 {
 	int	size_b;
 
-	size_b = stack_size(stack_b);
-	if (size_b > 0 || !already_sorted(stack_a))
+	size_b = lst_size(stack_b);
+	if (size_b > 0 || !is_sorted(stack_a))
 		write(1, "KO\n", 3);
 	else
 		write(1, "OK\n", 3);
