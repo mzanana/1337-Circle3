30fb7047fc90217faff0ee620234563482ce42f5
mzanana <mr.zananamohamed@gmail.com>
2025-04-06 00:01:50 +0000

    norminette fr_linkedlist.c

--- Push_Swap/Project/ft_linkedlist.c
+++ Push_Swap/Project/ft_linkedlist.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 19:56:47 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/04 22:03:43 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/06 00:01:28 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	lst_addback(t_stack **head, int num)
 {
 	t_stack	*new;
-	t_stack *tmp;
+	t_stack	*tmp;
 
 	new = malloc (sizeof(t_stack));
 	if (!head || !new)
@@ -29,25 +29,25 @@ void	lst_addback(t_stack **head, int num)
 	}
 	tmp = *head;
 	while (tmp->next)
-		tmp = tmp->next;	
+		tmp = tmp->next;
 	tmp->next = new;
 }
 
-int     lst_size(t_stack *stack)
+int	lst_size(t_stack *stack)
 {
-	int	ret;
-	t_stack *tmp;
-	
+	int		ret;
+	t_stack	*tmp;
+
 	if (!stack)
-		return 0;
-	tmp =  stack;
+		return (0);
+	tmp = stack;
 	ret = 0;
 	while (tmp)
 	{
 		ret++;
 		tmp = tmp->next;
 	}
-	return ret;
+	return (ret);
 }
 
 void	free_stack(t_stack **head)
@@ -65,16 +65,15 @@ void	free_stack(t_stack **head)
 	}
 }
 
-int     is_sorted(t_stack *stack)
+int	is_sorted(t_stack *stack)
 {
 	if (!stack || !stack->next)
-		return 1;
-
+		return (1);
 	while (stack->next)
 	{
 		if (stack->data > stack->next->data)
-			return 0;
-		stack = stack->next;		
+			return (0);
+		stack = stack->next;
 	}
-	return 1;
+	return (1);
 }
