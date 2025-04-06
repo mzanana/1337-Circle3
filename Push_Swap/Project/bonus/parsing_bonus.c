2690c10f74619d22c132fde62d455b1881e41bd4
mzanana <mr.zananamohamed@gmail.com>
2025-04-06 06:23:50 +0100

    parsing_bonus.c file

---
+++ Push_Swap/Project/bonus/parsing_bonus.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/06 06:21:59 by mzanana           #+#    #+#             */
+/*   Updated: 2025/04/06 06:22:17 by mzanana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker_bonus.h"
+
+void	ft_error(char	**str, t_stack	**st)
+{
+	write(2, "Error\n", 6);
+	if (str)
+		free_split(str);
+	if (st)
+		free_stack(st);
+	exit (1);
+}
+
+int	is_num(char c)
+{
+	if ((c >= '0' && c <= '9') || c == '-' || c == '+' || c == ' ')
+		return (1);
+	else
+		return (0);
+}
+
+int	is_valid(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!is_num(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_atoi(char *str, char **split, t_stack *stack)
+{
+	long	ret;
+	int		i;
+	int		sign;
+
+	if (!str)
+		return (0);
+	i = 0;
+	sign = 1;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	ret = 0;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		ret = ret * 10 + (str[i] - '0');
+		if ((ret * sign) > INT_MAX || (ret * sign) < INT_MIN)
+			ft_error(split, &stack);
+		i++;
+	}
+	return ((int)(ret * sign));
+}
+
+t_stack	*ft_parsing(char **av, int ac)
+{
+	t_stack	*stack;
+	char	**split;
+
+	int (i), (j), (num);
+	stack = NULL;
+	i = 1;
+	while (i < ac)
+	{
+		if (!is_valid(av[i]))
+			ft_error(NULL, &stack);
+		split = ft_split(av[i]);
+		if (!split)
+			ft_error(NULL, &stack);
+		j = 0;
+		while (split[j])
+		{
+			num = ft_atoi(split[j], split, stack);
+			if (!check_number(split[j++]) || num_duplicate(stack, num))
+				ft_error(split, &stack);
+			lst_addback(&stack, num);
+		}
+		free_split(split);
+		i++;
+	}
+	return (stack);
+}
