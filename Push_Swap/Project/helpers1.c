15afb7d7601e7770fb5e4a486ba9f3312d4ef58f
mzanana <mr.zananamohamed@gmail.com>
2025-04-06 00:23:59 +0000

    norminette helpers

--- Push_Swap/Project/helpers1.c
+++ Push_Swap/Project/helpers1.c
@@ -6,50 +6,50 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 02:45:05 by mzanana           #+#    #+#             */
-/*   Updated: 2025/03/28 00:58:59 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/06 00:23:44 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
-    int len;
+	int	len;
 
-    if (!str)
-        return ;
-    len = ft_strlen(str);
-    write(1, str, len);
+	if (!str)
+		return ;
+	len = ft_strlen (str);
+	write (1, str, len);
 }
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-    int i;
+	int	i;
 
-    if (!str)
-        return 0;
-    i = 0;
-    while (str[i])
-        i++;
-    return i;
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
 int	check_number(char *str)
 {
-	int i;
+	int	i;
 
 	if (!str)
-		return 0;
+		return (0);
 	i = 0;
 	if (str[i + 1] && (str[i] == '+' || str[i] == '-'))
 		i++;
 	while (str[i])
 	{
 		if (str[i] < '0' || str[i] > '9')
-			return 0;
+			return (0);
 		i++;
 	}
-	return 1;
+	return (1);
 }
 
 int	num_duplicate(t_stack *stack, int num)
@@ -57,8 +57,8 @@ int	num_duplicate(t_stack *stack, int num)
 	while (stack)
 	{
 		if (stack->data == num)
-			return 1;
+			return (1);
 		stack = stack->next;
 	}
-	return 0;
+	return (0);
 }
