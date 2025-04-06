964537d5375837af67574dd904b8cc4cc427c12d
mzanana <mr.zananamohamed@gmail.com>
2025-04-06 06:04:45 +0100

    the result function to check if the result stack is sorted or not

--- Push_Swap/Project/bonus/main_bonus.c
+++ Push_Swap/Project/bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 05:23:50 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/06 06:01:22 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/06 06:04:02 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,17 @@ void	apply_oper(t_stack **sta, t_stack **stb)
 	}
 }
 
+void	result(t_stack *stack_a, t_stack *stack_b)
+{
+	int	size_b;
+
+	size_b = stack_size(stack_b);
+	if (size_b > 0 || !already_sorted(stack_a))
+		write(1, "KO\n", 3);
+	else
+		write(1, "OK\n", 3);
+}
+
 int	main(int ac, char **av)
 {
 	t_stack		*sta;
@@ -90,4 +101,9 @@ int	main(int ac, char **av)
 	if (!sta)
 		ft_error(NULL, NULL);
 	apply_oper(&sta, &stb);
+	result (sta, stb);
+	if (stb)
+		free_stack(&stb);
+	free_stack(&sta);
+	return (0);
 }
