31a1697dcc271c21a014e3e9ba8ab34a71c4745e
mzanana <mr.zananamohamed@gmail.com>
2025-04-07 03:29:12 +0100

    bonus main function

--- Push_Swap/Project/bonus/main_bonus.c
+++ Push_Swap/Project/bonus/main_bonus.c
@@ -5,91 +5,96 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/06 05:23:50 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/06 06:50:14 by mzanana          ###   ########.fr       */
+/*   Created: 2025/04/07 03:28:42 by mzanana           #+#    #+#             */
+/*   Updated: 2025/04/07 03:28:49 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker_bonus.h"
 
 
-int	execute(char *oper, t_stack **sta, t_stack **stb)
+int	ft_strcmp(char *s1, char *s2)
 {
-	if (!ft_strcmp(oper, "sa\n"))
-		sa(sta);
-	else if (!ft_strcmp(oper, "sb\n"))
-		sb(stb);
-	else if (!ft_strcmp(oper, "ss\n"))
-		ss(sta, stb);
-	else if (!ft_strcmp(oper, "pa\n"))
-		pa(sta, stb);
-	else if (!ft_strcmp(oper, "pb\n"))
-		pb(sta, stb);
-	else if (!ft_strcmp(oper, "ra\n"))
-		ra(sta);
-	else if (!ft_strcmp(oper, "rb\n"))
-		rb(stb);
-	else if (!ft_strcmp(oper, "rr\n"))
-		rr(sta, stb);
-	else if (!ft_strcmp(oper, "rra\n"))
-		rra(sta);
-	else if (!ft_strcmp(oper, "rrb\n"))
-		rrb(stb);
-	else if (!ft_strcmp(oper, "rrr\n"))
-		rrr(sta, stb);
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (s1[i] - s2[i]);
+	}
+	return (s1[i] - s2[i]);
+}
+
+int	execute_instruction(char *cmd, t_stack **stack_a, t_stack **stack_b)
+{
+	if (!ft_strcmp(cmd, "sa\n"))
+		sa(stack_a);
+	else if (!ft_strcmp(cmd, "sb\n"))
+		sb(stack_b);
+	else if (!ft_strcmp(cmd, "ss\n"))
+		ss(stack_a, stack_b);
+	else if (!ft_strcmp(cmd, "pa\n"))
+		pa(stack_a, stack_b);
+	else if (!ft_strcmp(cmd, "pb\n"))
+		pb(stack_b, stack_a);
+	else if (!ft_strcmp(cmd, "ra\n"))
+		ra(stack_a);
+	else if (!ft_strcmp(cmd, "rb\n"))
+		rb(stack_b);
+	else if (!ft_strcmp(cmd, "rr\n"))
+		rr(stack_a, stack_b);
+	else if (!ft_strcmp(cmd, "rra\n"))
+		rra(stack_a);
+	else if (!ft_strcmp(cmd, "rrb\n"))
+		rrb(stack_b);
+	else if (!ft_strcmp(cmd, "rrr\n"))
+		rrr(stack_a, stack_b);
 	else
 		return (0);
 	return (1);
 }
 
-void	apply_oper(t_stack **sta, t_stack **stb)
+void	ft_check_moves(char *line, t_stack **sta, t_stack **stb)
 {
-	char	*op;
-
-	op = get_next_line(0);
-	while (op)
+	if (!line)
+		return ;
+	if (!execute_instruction(line, sta, stb))
 	{
-		if (!execute(op, sta, stb))
-		{
-			free(op);
-			write(2, "Error\n", 6);
-			get_next_line(-1);
-			free_stack(sta);
-			free_stack(stb);
-			exit(1);
-		}
-		free(op);
-		op = get_next_line(0);
+		write(2, "Error\n", 6);
+		free(line);
+		free_stack(sta);
+		free_stack(stb);
+		exit(1);
 	}
-}
-
-void	result(t_stack *stack_a, t_stack *stack_b)
-{
-	int	size_b;
-
-	size_b = lst_size(stack_b);
-	if (size_b > 0 || !is_sorted(stack_a))
-		write(1, "KO\n", 3);
-	else
-		write(1, "OK\n", 3);
+	free(line);
 }
 
 int	main(int ac, char **av)
 {
 	t_stack		*sta;
 	t_stack		*stb;
-	int			size;
-
+	char		*line;
+	
 	if (ac < 2)
 		return (0);
 	stb = NULL;
 	sta = ft_parsing(av, ac);
 	if (!sta)
 		ft_error(NULL, NULL);
-	apply_oper(&sta, &stb);
-	result (sta, stb);
-	if (stb)
-		free_stack(&stb);
+	while (1)
+	{
+		line = get_next_line(0);
+		ft_check_moves(line, &sta, &stb);
+		if (!line)
+			break ;
+	}
+	if (is_sorted(sta) && !stb)
+		write (1, "OK\n", 3);
+	else
+		write (1, "KO\n", 3);
 	free_stack(&sta);
-	return (0);
+	free_stack(&stb);
 }
