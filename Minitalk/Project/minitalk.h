626fcd64a5bf2856781e39be7e96838b6fb0f496
mzanana <mr.zananamohamed@gmail.com>
2025-04-09 18:21:13 +0100

    Header file of the project

---
+++ Minitalk/Project/minitalk.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 12:53:25 by mzanana           #+#    #+#             */
+/*   Updated: 2025/04/09 18:18:19 by mzanana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <signal.h>
+
+
+void	ft_header();
+void	ft_putnbr(int n);
+void	ft_putstr(char *str);
+
+#endif
