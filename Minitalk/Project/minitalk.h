c94ea4bd4549199a2856fe04daf605098528c1b0
mzanana <mr.zananamohamed@gmail.com>
2025-04-14 22:06:01 +0100

    Header file update

--- Minitalk/Project/minitalk.h
+++ Minitalk/Project/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 12:53:25 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/09 18:18:19 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/14 21:47:15 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,20 @@
 
 # include <unistd.h>
 # include <signal.h>
-
+# include <limits.h>
+# include <stdlib.h>
+//#include <bits/siginfo.h>
 
 void	ft_header();
 void	ft_putnbr(int n);
 void	ft_putstr(char *str);
+int		ft_atoi(char *str);
+
+void	ft_send (char c, int pid);
+void	ft_reset (int signum);
+
+void	ft_header(void);
+void	handler(int signum, siginfo_t *info, void *context);
+
 
 #endif
