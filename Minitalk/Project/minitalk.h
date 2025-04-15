0b8baa59a8927d78a1b16569da43ccf62f0dcfc1
mzanana <mr.zananamohamed@gmail.com>
2025-04-15 11:37:04 +0100

    time.h library

--- Minitalk/Project/minitalk.h
+++ Minitalk/Project/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 12:53:25 by mzanana           #+#    #+#             */
-/*   Updated: 2025/04/15 10:46:44 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/04/15 10:52:32 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
 # include <signal.h>
 # include <limits.h>
 # include <stdlib.h>
-//#include <bits/siginfo.h>
+# include <time.h>
+
+// #include <bits/siginfo.h>
 void	ft_header(void);
 void	ft_putnbr(int n);
 void	ft_putstr(char *str);
